6bd51b14773628aed592d16bd7e55755178fe441
Jérémy Jourdan <github@kema.dev>
2025-07-07 17:15:02 +0200

    up

--- go-k8s/tool/common/common.go
+++ go-k8s/tool/common/common.go
@@ -9,6 +9,9 @@ import (
 	"os/exec"
 	"strings"
 	"syscall"
+
+	"github.com/kemadev/ci-cd/pkg/filesfind"
+	kg "github.com/kemadev/ci-cd/pkg/git"
 )
 
 type cmd struct {
@@ -187,7 +190,86 @@ func runCITasks(args []string) error {
 
 func runGoTasks(args []string) error {
 	slog.Debug("Running Go tasks", slog.Any("args", args))
-	// Implement Go task logic here
+
+	if len(args) != 1 {
+		return fmt.Errorf(
+			"expected exactly one argument for Go tasks, got %d",
+			len(args),
+		)
+	}
+
+	task := args[0]
+	switch task {
+	case "update":
+		slog.Info("Updating Go modules")
+		mods, err := filesfind.FindFilesByExtension(filesfind.FilesFindingArgs{
+			Extension: "go.mod",
+			Recursive: true,
+		})
+		if err != nil {
+			return fmt.Errorf("error finding go.mod files: %w", err)
+		}
+		if len(mods) == 0 {
+			return fmt.Errorf("no go.mod files found in the current directory or subdirectories")
+		}
+		slog.Debug("Found go.mod files", slog.Any("mods", mods))
+		for _, mod := range mods {
+			slog.Debug("Updating Go module", slog.String("mod", mod))
+			cmd := exec.Command("go", "get", "-u", "./...")
+			cmd.Dir = mod
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+			if err := cmd.Run(); err != nil {
+				return fmt.Errorf("error updating Go module %s: %w", mod, err)
+			}
+			slog.Info("Updated Go module", slog.String("mod", mod))
+		}
+	case "tidy":
+		slog.Info("Tidying Go modules")
+		mods, err := filesfind.FindFilesByExtension(filesfind.FilesFindingArgs{
+			Extension: "go.mod",
+			Recursive: true,
+		})
+		if err != nil {
+			return fmt.Errorf("error finding go.mod files: %w", err)
+		}
+		if len(mods) == 0 {
+			return fmt.Errorf("no go.mod files found in the current directory or subdirectories")
+		}
+		slog.Debug("Found go.mod files", slog.Any("mods", mods))
+		for _, mod := range mods {
+			slog.Debug("Tidying Go module", slog.String("mod", mod))
+			cmd := exec.Command("go", "mod", "tidy")
+			cmd.Dir = mod
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+			if err := cmd.Run(); err != nil {
+				return fmt.Errorf("error tidying Go module %s: %w", mod, err)
+			}
+			slog.Info("Tidied Go module", slog.String("mod", mod))
+		}
+	case "init":
+		slog.Info("Initializing Go module")
+		basePath, err := kg.GetGitBasePath()
+		if err != nil {
+			return fmt.Errorf("error getting git repository base path: %w", err)
+		}
+		if basePath == "" {
+			return fmt.Errorf("error getting git repository base path")
+		}
+
+		cmd := exec.Command("go", "mod", "init", basePath)
+		cmd.Dir = "."
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		if err := cmd.Run(); err != nil {
+			return fmt.Errorf("error initializing Go module: %w", err)
+		}
+		slog.Info("Initialized Go module", slog.String("basePath", basePath))
+	default:
+		return fmt.Errorf("unknown Go task: %s", task)
+	}
+
 	return nil
 }
 
