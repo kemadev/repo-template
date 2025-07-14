70025527152515f2547eaae1e08882e493d05c85
Jérémy Jourdan <github@kema.dev>
2025-07-14 17:27:18 +0200

    up

--- go-k8s/tool/dev/docker-entrypoint-dev.sh
+++ go-k8s/tool/dev/docker-entrypoint-dev.sh
@@ -1,23 +1,10 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
-set -euo pipefail
+set -eu
 
-function main() {
-    if [[ -z "${GO_MAIN_FILE_DIR}" ]]; then
+main() {
+    if [ -z "${GO_MAIN_FILE_DIR:-}" ]; then
         echo "GO_MAIN_FILE_DIR is not set."
-        exit 1
-    fi
-
-
-    local SECRETS_DIR="/run/secrets"
-
-    if [[ ! -d "${SECRETS_DIR}" ]]; then
-        echo "Secrets directory '${SECRETS_DIR}' does not exist. Ensure that the secrets are mounted correctly."
-        exit 1
-    fi
-
-    if [[ ! -f "${SECRETS_DIR}/netrc" ]]; then
-        echo "The netrc file does not exist in '${SECRETS_DIR}'. Ensure that the secret is mounted correctly."
         exit 1
     fi
 
@@ -25,11 +12,11 @@ function main() {
 
     go build -o /app "${GO_MAIN_FILE_DIR}"
 
-    exec /app "${@}" || {
-        echo "Failure running the application, now sleeping."
-        sleep inifinity
-        exit 1
-    }
+    /app "${@}"
+
+    echo "Failure running the application, now sleeping."
+    sleep infinity
+    exit 1
 }
 
 main "${@}"
