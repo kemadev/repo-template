d0ff160a57a85af84467b0693b6c72ab149ff3f6
Jérémy Jourdan <github@kema.dev>
2025-07-14 20:42:51 +0200

    up

--- go-k8s/tool/dev/docker-entrypoint-dev.sh
+++ go-k8s/tool/dev/docker-entrypoint-dev.sh
@@ -3,28 +3,12 @@
 set -eu
 
 main() {
-    if [ -z "${GO_MAIN_FILE_DIR:-}" ]; then
-        echo "GO_MAIN_FILE_DIR is not set."
-        exit 1
-    fi
-
-    if [ -z "${HOME:-}" ]; then
-        echo "HOME is not set."
-        exit 1
-    fi
-
-    cp /run/secrets/netrc ~/.netrc
-
-    BUILD_TARGET_PATH="${HOME}/app"
-
-    go build -o "${BUILD_TARGET_PATH}" "${GO_MAIN_FILE_DIR}"
-
     set +e
-    "${BUILD_TARGET_PATH}" "${@}"
+    /app "${@}"
     EXIT_CODE=$?
     set -e
 
-    if [ ${EXIT_CODE} -eq 0 ]; then
+    if [ "${EXIT_CODE}" -eq 0 ]; then
         echo "Application exited successfully."
     else
         echo "Application exited with code ${EXIT_CODE}."
